ef99c65fb80b7902d0dcf3ebbbaa27ee62461d32
ajitsariakarishma <karishmaajitsaria@gmail.com>
2020-05-17 20:49:07 +0100

    second commit

---
+++ src/ExpressionOfSUmOfPrimeNumbers.java
@@ -0,0 +1,42 @@
+public class ExpressionOfSUmOfPrimeNumbers {
+    public static void main(String[] args) {
+
+     ExpressNumberInExpressionOfPrimeNumbers(34);
+    ExpressNumberInExpressionOfPrimeNumbers(11);
+
+
+
+
+
+    }
+    public static void ExpressNumberInExpressionOfPrimeNumbers(int num){
+         boolean flag = false;
+        for(int i=2;i<num/2;i++){//9
+            if(isPrime(i)){
+                if (isPrime(num - i)) {
+                    System.out.println(i+" + "+(num-i)+" = "+num);
+                     flag = true;
+                }
+            }
+        }
+        if(!flag){
+            System.out.println(num+" cannot be expressed in sum of prime number expression ");
+        }
+
+    }
+
+    public static boolean isPrime(int number){
+        boolean isPrime = true;
+        for(int i = 2; i<=number/2;i++){
+            if (number % i == 0) {
+                isPrime= false;
+                break;
+            }
+            }
+        return isPrime;
+        }
+    }
+
+
+
+
