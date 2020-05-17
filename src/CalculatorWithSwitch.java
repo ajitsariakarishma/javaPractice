d1f8e7567081699210c20cfb095f26180fd60c3f
ajitsariakarishma <karishmaajitsaria@gmail.com>
2020-05-17 19:52:33 +0100

    second commit

---
+++ src/CalculatorWithSwitch.java
@@ -0,0 +1,47 @@
+public class CalculatorWithSwitch {
+    public static void main(String[] args) {
+      calculator(4,2,'/');
+      calculator(45,78,'-');
+      calculator(50,2,'9');
+
+    }
+
+    public static void calculator(double number1, double number2, char operator) {
+         double result=0;
+        switch (operator){
+            case '+':
+                result=number1+number2;
+                System.out.println(number1+" "+operator+" "+number2+" = "+result);
+                break;
+            case '-':
+                result=number1-number2;
+                System.out.println(number1+" "+operator+" "+number2+" = "+result);
+                break;
+            case '*':
+                result=number1*number2;
+                System.out.println(number1+" "+operator+" "+number2+" = "+result);
+                break;
+            case '/':
+                result=number1/number2;
+                System.out.println(number1+" "+operator+" "+number2+" = "+result);
+                break;
+
+                default:
+                    System.out.println("plese enter correct operator");
+                    break;
+        }
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
