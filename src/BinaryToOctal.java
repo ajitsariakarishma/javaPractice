2603f3ac77b8b0e1f8c4a87cc553f66a932a1ce4
ajitsariakarishma <karishmaajitsaria@gmail.com>
2020-05-18 23:23:11 +0100

    second commit

--- src/BinaryToOctal.java
+++ src/BinaryToOctal.java
@@ -4,8 +4,15 @@ public class BinaryToOctal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a binary number to get its octal value: ");
+        /*string is parsed to integer via parseInt() function and radix specifies hich numeral system to be used
+        here radix is 2 that means use binary system because binary syatem has base 2
+        Integer. toOctalString() method is a built-in function in Java that is used to return a string representation
+        of the integer argument as an unsigned integer in base 8.
+        unsigned integer means a non negative number.
+         */
+
         int number = Integer.parseInt(scanner.next(),2);
-        String octal = Integer.toOctalString(number);
+         String octal = Integer.toOctalString(number);
 
         System.out.println("Octal value of  is "+octal);
 
